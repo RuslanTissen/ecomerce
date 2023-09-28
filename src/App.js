8d47dc6e6356548cdae62907c5e0677f164f7cff
SFERAS <rthyssen73@gmail.com>
2023-09-28 14:18:13 -0300

    start new day

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Header from './Header.jsx';
 import Home from "./Home.jsx"
+import Checkout from "./Checkout.jsx"
 
 function App() {
 	return (
 		<div className="app">
 			<Header />
 			<Routes>
-				<Route path='*' element={<Home />}>
-					{/* <Route path='home' element={<Home />} /> */}
-					{/*  */}
-					{/*  */}
+				<Route path='*' element={<Home />}></Route>
+				{/* <Route path='home' element={<Home />} /> */}
+				<Route path='checkout' element={<Checkout />} />
 
-				</Route>
 			</Routes>
 		</div>
 	);
