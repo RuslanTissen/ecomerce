28d52baf99b9e6fae38656170bd1c9bb197becc3
SFERAS <rthyssen73@gmail.com>
2023-09-27 22:06:38 -0300

    images added

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,12 @@ import "./Product.jsx"
 import Product from './Product.jsx'
 import living from "../src/images/living3.jpg"
 import book from "../src/images/book.png"
+import airPods from "../src/images/Apple_AirPods.png"
+import headphone from "../src/images/Bose_Headphones_700.png"
+import pots from "../src/images/Pots_and_Pans.png"
+import Weight from "../src/images/Weight.png"
+import coffee from "../src/images/Coffee_Maker.png"
+
 
 function Home() {
 	return (
@@ -18,18 +24,18 @@ function Home() {
 
 				<div className="home__row">
 					<Product id="123564872" title="The Lean startup" price={19.99} image={book} rating={4}/>
-					<Product id="226517899" title="Apple AirPods (2nd Generation) Wireless Earbuds with Lightning Charging Case Included. Over 24 Hours of Battery Life, Effortless Setup. Bluetooth Headphones for iPhonestartup" price={19.99} image={book} rating={5}/>
+					<Product id="226517899" title="Apple AirPods (2nd Generation) Wireless Earbuds with Lightning Charging Case Included. Over 24 Hours of Battery Life, Effortless Setup. Bluetooth Headphones for iPhonestartup" price={909.00} image={airPods} rating={5}/>
 
 				</div>
 
 				<div className="home__row">
-					<Product />
-					<Product />
-					<Product />
+				<Product id="165465456" title="Bose Headphones 700, Noise Cancelling Bluetooth Over-Ear Wireless Headphones with Built-In Microphone for Clear Calls and Alexa Voice Control, Black" price={299.99} image={headphone} rating={4}/>
+				<Product id="328547931" title="Ayesha Curry Home Collection Hard Anodized Nonstick Cookware Pots and Pans Set, 9 Piece, Charcoal Gray" price={91.26} image={pots} rating={5}/>
+				<Product id="965231845" title="Portzon 8 Colors Options Compatible with Set of 2 Rubber Dumbbell Weight, Anti-Slip, Anti-roll, Hex Shape" price={12.83} image={Weight} rating={5}/>
 				</div>
 
 				<div className="home__row">
-					<Product />
+				<Product id="548923157" title="Café Specialty Drip Coffee Maker | 10-Cup Insulated Thermal Carafe | WiFi Enabled Voice-to-Brew Technology | Smart Home Kitchen Essentials | SCA Certified, Barista-Quality Brew | Matte Black" price={367.00} image={coffee} rating={3}/>
 				</div>
 
 			</div>
