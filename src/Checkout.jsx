32828b48a4acf7227cd3707018f1e9b3dfd9bd95
SFERAS <rthyssen73@gmail.com>
2023-09-28 22:07:42 -0300

    css seved

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import "./Checkout.css"
+import banner from "../src/images/banner.jpg"
 
 function Checkout() {
 	return (
 		<div className='checkout'>
-			<h1>Smesh the button</h1>
+			<div className="checkout__left">
+				<div className="checkout__ad">
+					{/* <img src={banner} alt="" className="checkout__add" /> */}
+				</div>
+				<div>
+					<h2 className="checkout__title">	Your shopping basket	</h2>
+				</div>
+			</div>
+
+			<div className="checkout__right">
+				<h2>The subtitle will be here</h2>
+			</div>
+
 		</div>
 	)
 }
