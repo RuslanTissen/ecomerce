a6cff82c50b35a983f61d9057547c29f2ce05ce9
SFERAS <rthyssen73@gmail.com>
2023-10-07 00:19:48 -0300

    basket works

--- src/componets/Subtotal.jsx
+++ src/componets/Subtotal.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Subtotal.css"
 import CurrencyFormat from 'react-currency-format'
 import { useStateValue } from '../StateProvider'
+import { getBasketTotal } from './reducer'
 
 function Subtotal(price) {
 	const [{basket}, dispatch] = useStateValue()
@@ -12,7 +13,7 @@ function Subtotal(price) {
 				renderText={(value) => (
 					<>
 						<p>
-							Subtotal ({basket.length} items): <strong>$ {basket[0].price}</strong>
+							Subtotal ({basket.length} items): <strong>$ {value}</strong>
 						</p>
 						<small className='subtotal__gift'>
 							<input type='checkbox' /> This order contain a gift
@@ -20,7 +21,7 @@ function Subtotal(price) {
 					</>
 				)}
 				decimalScale={2}
-				value={0}
+				value={getBasketTotal(basket)}
 				displayType={"text"}
 				thousandSeparator={true}
 				prefix={"$"}
